b0a7fba67f4324660ed07332a22c6ac8683b0fae
salahuddin <salahuddin@sharetrip.net>
2024-10-27 23:59:17 +0600

    added dto

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,15 @@ import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { JwtModule } from "@nestjs/jwt";
+import { ConfigModule } from "@nestjs/config";
+import { PrismaModule } from "src/prisma/prisma.module";
 
 @Module({
-    imports:[JwtModule.register({})],
+    imports:[
+        PrismaModule,
+        JwtModule.register({}),
+        ConfigModule.forRoot({isGlobal: true})
+    ],
     controllers: [AuthController],
     providers: [AuthService]
 })
